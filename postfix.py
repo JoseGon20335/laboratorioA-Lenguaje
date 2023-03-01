63f664be362fd6358bb837641fcbca80b8b21403
JoseGonzalez098 <gon20335@uvg.edu.gt>
2023-02-28 18:46:19 -0600

    post

--- postfix.py
+++ postfix.py
@@ -1,5 +1,5 @@
 def passToPostFix(redex):
-    specials = {'*': 5, '+': 4, '?': 3, '.': 2, '|': 1}
+    specials = {'(': 1, "(": 1, '|': 2, '.': 3, '*': 4, '+': 4, '?': 4}
     alfabetoA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                  's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -14,25 +14,30 @@ def passToPostFix(redex):
             fixRedex += c + '.'
         elif i+1 != len(redex) and c == ')' and redex[i+1] in alfabetoA:
             fixRedex += c + '.'
+        elif i+1 != len(redex) and c == '?' and redex[i+1] in alfabetoA:
+            fixRedex += c + '.'
+        elif i+1 != len(redex) and c == '*' and redex[i+1] in alfabetoA:
+            fixRedex += c + '.'
+        elif i+1 != len(redex) and c == '+' and redex[i+1] in alfabetoA:
+            fixRedex += c + '.'
         else:
             fixRedex += c
 
-    print(fixRedex)
+    print('a.(a?.b*|c+).b|b.a.a : ', fixRedex)
     for i, c in enumerate(fixRedex):
-        print(c)
         if c in alfabetoA:
-            if i+1 != len(fixRedex) and c in alfabetoA and fixRedex[i+1] in alfabetoA:
-                resultPostFix += c + '.'
-            else:
-                resultPostFix += c
+            resultPostFix += c
         elif c == '(':
             stack.append(c)
         elif c == ')':
-            while stack[-1] != '(':
+            while stack and stack[-1] != '(':
                 resultPostFix += stack.pop()
             stack.pop()
         else:
-            while stack and stack[-1] != '(' and specials.get(c, 0) <= specials.get(stack[-1], 0):
+            # while stack and stack[-1] != '(' and specials.get(c, 0) <= specials.get(stack[-1], 0):
+            #     resultPostFix += stack.pop()
+            # stack.append(c)
+            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
                 resultPostFix += stack.pop()
             stack.append(c)
 
